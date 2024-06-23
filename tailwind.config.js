f57cf7248d8e767684ef7aa55ad3d0eb04da5bc4
karanhv <157357048+karanhv@users.noreply.github.com>
2024-06-23 13:39:32 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        customBlue: '#003366',
+      },
+    },
+  },
+  plugins: [],
+}
+
